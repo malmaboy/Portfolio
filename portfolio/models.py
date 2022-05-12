07e32e8c79f6d8adc26ce4a9440e8dc9f071275f
Hugo Carvalho <huguinho66@live.com>
2022-05-12 22:22:44 +0100

    add subjects

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class subjects:
+    def __init__(self,name, year,semester,etcs,lective_year,topics,
+                 rank,prof,projects):
+        self.name = name
+        self.year = year
+        self.semester = semester
+        self.etcs = etcs
+        self.lective_year = lective_year
+        self.topics = topics
+        self.rank = rank
+        self.prof = prof
+        self.projects = projects
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,8 @@ import datetime
 
 from django.shortcuts import render
 
+from portfolio import models
+
 
 def home_page_view(request):
     agora = datetime.datetime.now()
@@ -18,7 +20,23 @@ def home_page_view(request):
 
 
 def about_view(request):
-    return render(request, 'portfolio/about.html')
+    # Subjcts
+    lp2Topics = ['SOLID', ' Design Patterns', 'Delegates', 'file Management',
+                 'Threads', 'Events', 'Atributes']
+    lp2Projects = ['https://github.com/malmaboy/wolf_and_sheepsLP2',
+                   'https://github.com/JMatos1221/LP2_Recurso',
+                   'https://github.com/malmaboy/NasaProject_LP2',
+                   'https://github.com/malmaboy/wolf_and_sheepsLP2']
+    LP2 = models.subjects("Linguagens da Programação 2", "2nd",
+                          "1st Semester", 6, "3rd", lp2Topics, 4,
+                          "Nuno Fachada", lp2Projects)
+
+    content = {
+        'LP2name': LP2.name,
+        'LP2Topics': LP2.topics
+    }
+
+    return render(request, 'portfolio/about.html', content)
 
 
 def contact_view(request):
